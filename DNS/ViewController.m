b03250c98e626ac509593b1b803e839ddaad6a29
WENTAO XING <a778758944@gmail.com>
2018-11-29 13:03:06 +0800

    Initial Commit

---
+++ DNS/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  DNS
+//
+//  Created by WENTAO XING on 2018/11/29.
+//  Copyright © 2018 WENTAO XING. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ DNS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DNS
+//
+//  Created by WENTAO XING on 2018/11/29.
+//  Copyright © 2018 WENTAO XING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
